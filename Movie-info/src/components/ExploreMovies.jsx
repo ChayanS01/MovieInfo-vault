cffdef34c7260e50fb476a1832fdf345b5cce386
Chayan Kumar Sen <95164355+ChayanS01@users.noreply.github.com>
2024-09-30 17:22:05 +0530

    Update ExploreMovies.jsx

--- Movie-info/src/components/ExploreMovies.jsx
+++ Movie-info/src/components/ExploreMovies.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
-import Moviecard from './Moviecard'
 import ExploreMovieCard from './ExploreMovieCard';
 // import ExploreMovieCard from './ExploreMovieCard';
+
 
 function ExploreMovies() {
   const url = "https://omdbapi.com/?s=";
@@ -34,7 +33,7 @@ function ExploreMovies() {
           setError(null);                       // Clear the error message
         } else {
           setSearchResults([]);                     // Clear the search results
-          setError('Please,Enter Correct Movie Name'); // Set the error message
+          setError('No Movie found!'); // Set the error message
         }
       })
       .catch(error => {
@@ -42,26 +41,30 @@ function ExploreMovies() {
         setError('An error occurred while searching for movies');
       });
   }
+  
 
   return (
     <form className='grid justify-center grid-wrap m-10'>
-      <h1 className='text-white flex justify-center text-4xl pb-8 font-bold ' id='heading'>Explore Movies</h1>
-      <input type="text" placeholder='Search Movies' onChange={handleSubmit} onFocus={handleFocus}
-      onBlur={handleBlur} value={title} className='h-[3rem] w-[30rem] p-4 rounded-lg text-black  ' />
+      <h1 className='text-gray-300 flex justify-center text-4xl pb-8 font-bold underline underline-offset-8 decoration-gray-600' id='heading'>Explore Movies</h1>
+      {/* <input type="text" placeholder='Search Movies' onChange={handleSubmit} onFocus={handleFocus}
+      onBlur={handleBlur} value={title} className='h-[3rem] w-[30rem] p-4 rounded-lg text-black ' /> */}
+      <div class="flex justify-center items-center">
+  <input type="text" placeholder="Search Movies" onChange={handleSubmit} onFocus={handleFocus}
+    onBlur={handleBlur} value={title} class="h-[3rem] w-[30rem] p-4 m-2 rounded-lg text-black" />
+</div>
+
       <div className='flex justify-center m-5'>
-        <button onClick={handleSearch} className='h-[3rem] w-[15rem] bg-blue-500 rounded-md'>Search</button>
+        <button onClick={handleSearch} className='h-[3rem] w-[15rem] bg-blue-500 rounded-md mt-10'>Search</button>
       </div>
       {
         error ? (
-          <p className='text-red-500 text-center font-semibold text-4xl'>{error}</p>
+          <p className='text-red-500 bg-white rounded p-1 w-[20rem] mt-2 text-center ml-24 text-xl'>{error}</p>
         ) :
-          <div className='grid grid-cols-3 gap-4'>
+          <div className='shelf-list'>
             {searchResults.map((movie) => (
-              
               <ExploreMovieCard  movie={movie} />
             ))}
           </div>
-        
       }
     </form>
   )
