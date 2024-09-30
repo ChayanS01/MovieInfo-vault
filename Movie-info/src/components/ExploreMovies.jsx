38506ffc6e4b2fa44e0187674e4b375f1a0f0b49
Chayan Kumar Sen <95164355+ChayanS01@users.noreply.github.com>
2024-09-30 15:11:16 +0530

    Update ExploreMovies.jsx

--- Movie-info/src/components/ExploreMovies.jsx
+++ Movie-info/src/components/ExploreMovies.jsx
@@ -1,18 +1,3 @@
-import React from 'react'
-
-function ExploreMovies() {
-  return (
-    <div>
-        
-    </div>
-  )
-}
-
-export default ExploreMovies
-
-//  ExploreMovies.jsx
-
-
 import React, { useState } from 'react';
 import axios from 'axios';
 
@@ -31,6 +16,14 @@ function ExploreMovies() {
   const handleSubmit = (e) => {
     setTitle(e.target.value); // Update the title state when the input value changes
   }
+  const handleFocus = (e) => {
+    e.target.classList.add('search-focused');
+  };
+
+  // Function to remove the CSS class when search bar loses focus
+  const handleBlur = (e) => {
+    e.target.classList.remove('search-focused');
+  };
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -53,9 +46,10 @@ function ExploreMovies() {
   return (
     <form className='grid justify-center grid-wrap m-10'>
       <h1 className='text-white flex justify-center text-4xl pb-8 font-bold ' id='heading'>Explore Movies</h1>
-      <input type="text" placeholder='Search Movies' onChange={handleSubmit} value={title} className='h-[3rem] w-[30rem] outline-none p-4 rounded-lg flex justify-center text-black' />
+      <input type="text" placeholder='Search Movies' onChange={handleSubmit} onFocus={handleFocus}
+      onBlur={handleBlur} value={title} className='h-[3rem] w-[30rem] p-4 rounded-lg text-black  ' />
       <div className='flex justify-center m-5'>
-        <button onClick={handleSearch} className='h-[3rem] w-[10rem] bg-blue-500 rounded-xl'>Search</button>
+        <button onClick={handleSearch} className='h-[3rem] w-[15rem] bg-blue-500 rounded-md'>Search</button>
       </div>
       {
         error ? (
@@ -74,95 +68,3 @@ function ExploreMovies() {
 }
 
 export default ExploreMovies;
-
-// ExploreMovieCard.jsx
-import React from 'react';
-import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
-
-function ExploreMovieCard({ movie }) {
-  const navigate = useNavigate();
-
-  const onMovieClick = async () => {
-    const url = `https://omdbapi.com/?i=${movie.imdbID}&apikey=54b966e3`;
-    try {
-      const response = await axios.get(url);
-      const movieDetails = response.data;
-      navigate('/exploremoviedetail', { state: { movieDetails } });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
-      <div>
-      <ul className='shelf-list'>
-        
-          
-          <li key={movie.id} onClick={() => onMovieClick(movie)}>
-            <img className='moviecard-1' src={movie.Poster} alt={movie.title} />
-            <p className='movie-name'>{movie.Title}</p>
-            <p className='movie-genre'>{movie.genre}</p>
-            <div className='line-break'>
-              </div>
-          </li>
-     
-      </ul>
-      
-    </div>
-  );
-}
-
-export default ExploreMovieCard;
-
-// ExploreMOviesDetail.jsx
-
-import {React,useState} from 'react';
-
-import { useLocation } from "react-router-dom";
-import appletv from '../apple-tv logo.png'
-import imdbicon from '../imdb icon.png'
-
-
-function ExploreMovieDetail({ movie, onClose }) {
-    const location = useLocation();
-    const movieDetails = location.state.movieDetails;
-
-    const [selectedMovie, setSelectedMovie] = useState(null);
-    const handleClosePopup = () => {
-        setSelectedMovie(null);
-      };
-
-
-  return (
-    <div>
-      <div className="popup" id='popup'>
-        <div className="overlay" onClick={onClose}></div>
-        <div className="popup-content">
-          <img className='apple-logo' src={appletv} alt="" />
-          <h1 className='movie-title'>{movieDetails.Title}</h1>
-          <img className='box-image' src={movieDetails.Poster} alt="" />
-          <p className='movie-description'>{movieDetails.Plot}</p>
-          <p>Genre: {movieDetails.Genre}</p>
-          <p>First Episode Date: {movieDetails.Released}</p>
-          <p>Directed by: {movieDetails.Director}</p>
-          <p>Created by: {movieDetails.Writer}</p>
-          <div className='smallimage-container'>
-          <img id='movie-rating' className='imdb-icon' src={imdbicon}  alt="IMDB" />
-          <p class="imdb-text">{movieDetails.imdbRating}</p>
-          {/* <img id='movie-rating' className='rotten-tomatoes' src={movie.rottentomatoes} alt="Rotten Tomatoes" /> */}
-          {/* <p class="rotten-tomatoes-text">{movie.rottentomatorating}</p> */}
-          </div>
-          <button className='controls' onClick={handleClosePopup}>Close</button>
-          <button className='watchlist-btn'>Add to Watchlist</button>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default ExploreMovieDetail;
-
-// App.jsx"
-// add one route
- <Route path="/exploremoviedetail" element={<ExploreMovieDetail />} />
